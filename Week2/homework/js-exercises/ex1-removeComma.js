9340a8a92fc61773be14ecde1455e0e425571cca
Saoud <69368480+Oudy94@users.noreply.github.com>
2020-09-22 20:38:28 +0200

    remove week2 exercise from week1-Saoud branch

--- Week2/homework/js-exercises/ex1-removeComma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let myString = "hello,this,is,a,difficult,to,read,sentence";
-
-console.log(myString.length);
-
-myString = myString.replace(/,/g, " "); //To replace every comma "," with a space .
-
-console.log(myString);
