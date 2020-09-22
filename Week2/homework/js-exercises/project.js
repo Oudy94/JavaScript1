b6ee2d4471c7b32811b6708e65d7e508034891f6
Saoud <69368480+Oudy94@users.noreply.github.com>
2020-09-22 23:46:52 +0200

    update console.log at project.js

--- Week2/homework/js-exercises/project.js
+++ Week2/homework/js-exercises/project.js
@@ -1,20 +1,23 @@
 "use strict";
 
-calcGrades(49); // Here we assign the student mark as a parameter to the function to calculate the student grade
+let grade = 60; // Here we assign the student mark as a parameter to the calcGrades function to calculate the student grade
 
 function calcGrades(grade){
     
     if (grade >= 90) //Check if the mark is 90 or more 
-        return console.log(`you got a A (${grade}%)!`); //calulate the student grade
+        return `you got a A (${grade}%)!`; //calulate the student grade
     else if (grade >= 80) //Check if the mark is 80 or more 
-        return console.log(`you got a B (${grade}%)!`);
+        return `you got a B (${grade}%)!`;
     else if (grade >= 70) //Check if the mark is 70 or more 
-        return console.log(`you got a C (${grade}%)!`);
+        return `you got a C (${grade}%)!`;
     else if (grade >= 60) //Check if the mark is 60 or more 
-        return console.log(`you got a D (${grade}%)!`);
+        return `you got a D (${grade}%)!`;
     else if (grade >= 50) //Check if the mark is 50 or more 
-        return console.log(`you got a E (${grade}%)!`);
+        return `you got a E (${grade}%)!`;
     else // any mark less than 50
-        return console.log(`you got a F (${grade}%)!`);
+        return `you got a F (${grade}%)!`;
+
 }
+
+console.log(calcGrades(grade));
 
